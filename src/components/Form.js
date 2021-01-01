9a2528f8cb7545085f1c515a268f0a3d23792c95
SHAMBASHIB MAJUMDAR <shambashibmajumdar2000@gmail.com>
2021-01-01 18:48:28 +0530

    commiting in the Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,7 +3,9 @@ import React from 'react';
 
 const Form = () => {
 
-
+    const inputTextHandler = (e) => {
+        console.log(e);
+    };
     return(
         <form>
             <input type="text" className="todo-input" />
