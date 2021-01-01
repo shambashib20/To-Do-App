50796ebb509b8e543d8612e42594d5864570a918
SHAMBASHIB MAJUMDAR <shambashibmajumdar2000@gmail.com>
2021-01-01 18:26:50 +0530

    Made the TODOLIST component.

---
+++ src/components/ToDolist.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ToDolist = () => {
+    return (
+        <div className="todo-container">
+            <ul className="todo-list"></ul>
+        </div>
+    );
+};
+
+export default ToDolist;
