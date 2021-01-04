9f9b78e40048ef0506a52b5f66b7e1fb9ab3ee20
SHAMBASHIB MAJUMDAR <shambashibmajumdar2000@gmail.com>
2021-01-04 14:28:13 +0530

    creation of todo.js and changes

---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Todo = () => {
+    return(
+        <div className="todo">
+            <li classNam="todo-item"></li>
+        </div>
+    );
+}
+
+export default Todo;
